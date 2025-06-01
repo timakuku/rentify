428842a1100c80aa8a33e9e79b68366fb939e55f
timakuku <timakubatbekov6@gmail.com>
2025-06-01 20:53:46 +0600

    Update authMiddleware.js

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -3,14 +3,20 @@ const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-  const auth = req.headers.authorization;
-  if (!auth) return res.status(401).json({ message: 'Нет токена' });
+const authHeader = req.headers.authorization;
 
-  try {
-    const decoded = jwt.verify(auth.split(' ')[1], process.env.JWT_SECRET);
-    req.user = decoded;
-    next();
-  } catch {
-    res.status(401).json({ message: 'Неверный токен' });
-  }
+if (!authHeader?.startsWith('Bearer ')) {
+return res.status(401).json({ message: 'Нет токена' });
+}
+
+const token = authHeader.split(' ')[1];
+
+try {
+const decoded = jwt.verify(token, process.env.JWT_SECRET);
+req.user = decoded;
+next();
+} catch (err) {
+res.status(401).json({ message: 'Неверный токен' });
+}
 };
+
