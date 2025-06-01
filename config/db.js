ebdaf421a5e444b0b0c53618e239c670f1d5c575
timakuku <timakubatbekov6@gmail.com>
2025-06-02 00:12:42 +0600

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
+
     });
     console.log('✅ MongoDB подключен!');
   } catch (err) {
