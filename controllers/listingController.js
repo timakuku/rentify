558199bf1f2e1910d29d9cd908a88e1e21eb364d
timakuku <timakubatbekov6@gmail.com>
2025-06-02 22:38:12 +0600

    2

--- controllers/listingController.js
+++ controllers/listingController.js
@@ -1,4 +1,5 @@
 const Listing = require('../models/Listing');
+const imagekit = require("../config/imagekit");
 
 exports.getAllListings = async (req, res, next) => {
   try {
@@ -9,7 +10,35 @@ exports.getAllListings = async (req, res, next) => {
   }
 };
 
+exports.createListing = async (req, res) => {
+  try {
+    const { title, description, price, city, address } = req.body;
+    const file = req.files[0]; // предположим, первый файл
 
+    // Загружаем в ImageKit
+    const uploadResponse = await imagekit.upload({
+      file: file.buffer, // сам файл
+      fileName: file.originalname,
+      folder: "/rentify-images"
+    });
+
+    const listing = new Listing({
+      title,
+      description,
+      price,
+      city,
+      address,
+      images: [uploadResponse.url],
+      user: req.user.id
+    });
+
+    await listing.save();
+    res.status(201).json(listing);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Ошибка при создании объявления" });
+  }
+};
 
 exports.createListing = async (req, res, next) => {
 try {
