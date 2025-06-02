48f0b79f34983ed30a7e56fdfde88aa349451232
timakuku <timakubatbekov6@gmail.com>
2025-06-03 02:19:35 +0600

    Update listingController.js

--- controllers/listingController.js
+++ controllers/listingController.js
@@ -1,21 +1,17 @@
 const Listing = require('../models/Listing');
 const imagekit = require("../config/imagekit");
 
-exports.getAllListings = async (req, res, next) => {
-  try {
-    const listings = await Listing.find();
-    res.json(listings);
-  } catch (err) {
-    next(err);
-  }
-};
+
 
 exports.createListing = async (req, res) => {
   try {
     const { title, description, price, city, address, type } = req.body;
     const files = req.files;
 
-    // Загружаем каждую картинку в ImageKit и собираем URL
+    if (!files || files.length === 0) {
+      return res.status(400).json({ message: "Файлы не загружены!" });
+    }
+
     const imageUrls = await Promise.all(
       files.map(async (file) => {
         const result = await imagekit.upload({
@@ -23,6 +19,7 @@ exports.createListing = async (req, res) => {
           fileName: file.originalname,
           folder: "/rentify-images"
         });
+        console.log("Загружен файл:", result.url);
         return result.url;
       })
     );
@@ -35,16 +32,17 @@ exports.createListing = async (req, res) => {
       address,
       type,
       images: imageUrls,
-      user: req.user.id // если у тебя в req.user хранится id
+      user: req.user.id
     });
 
     await listing.save();
     res.status(201).json(listing);
   } catch (err) {
-    console.error(err);
+    console.error("Ошибка при создании объявления:", err);
     res.status(500).json({ message: "Ошибка при создании объявления" });
   }
 };
+
 
 // controllers/listingController.js
 
