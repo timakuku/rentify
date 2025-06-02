38d294221915e5d4c7de1d124b9f2bdceb764dff
timakuku <timakubatbekov6@gmail.com>
2025-06-02 23:19:22 +0600

    2

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-
+      
     });
     console.log('✅ MongoDB подключен!');
   } catch (err) {
